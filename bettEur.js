739ea2cfcfc3f4896471f450f0d4d3d662fd5e4c
Pepijn de Kruijff <pepijn@MacBook-Pro-van-Pepijn.local>
2023-12-02 12:12:58 +0100

    chatbot

--- bettEur.js
+++ bettEur.js
@@ -1,7 +1,63 @@
 // darkmode
 let darkMode = localStorage.getItem('darkMode'); 
 
+
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const chatButton = document.querySelector('#chatbot-toggle');
+const chatbotClose = document.querySelector('#chatbot-close');
+
+const jsonData = {
+  "questions_and_answers": [
+  {
+  "question": "Hello",
+  "answer": "Hi, feel free to ask any questions you have."
+  },
+  {
+  "question": "What services are there for disabled students?",
+  "answer": "Disabled students can access academic adjustments, assistive technology, and accessible transportation services."
+  },
+  {
+  "question": "How to request academic accommodations?",
+  "answer": "To request academic accommodations, reach out to the Disability Support Office and submit disability documentation."
+  },
+  {
+  "question": "Any disability scholarships?",
+  "answer": "Disability scholarships are available. Visit our financial aid website for more information."
+  },
+  {
+  "question": "Is the campus wheelchair friendly?",
+  "answer": "The campus is wheelchair friendly, featuring ramps, elevators, and accessible restrooms."
+  },
+  {
+  "question": "Can I get transport help?",
+  "answer": "Transport help is provided through accessible shuttles and assistance services."
+  },
+  {
+  "question": "What mental health support is available?",
+  "answer": "Available mental health support includes counseling, support groups, and workshops."
+  },
+  {
+  "question": "How to get a housing accommodation?",
+  "answer": "To get a housing accommodation, contact the housing office with your disability documentation."
+  },
+  {
+  "question": "Any student groups for disabilities?",
+  "answer": "There are student organizations focused on disabilities."
+  },
+  {
+  "question": "What assistive tech is available?",
+  "answer": "Available assistive technology includes screen readers, voice recognition software, and more."
+  },
+  {
+  "question": "How to contact Disability Support Office?",
+  "answer": "The Disability Support Office can be contacted via email, phone, or by visiting their campus office."
+  }
+  ]
+  }
+
+
+const questions = jsonData.questions_and_answers.map(item => item.question);
+const answers = jsonData.questions_and_answers.map(item => item.answer);
 
 const enableDarkMode = () => {
   document.body.classList.add('darkmode');
@@ -17,6 +73,40 @@ if (darkMode === 'enabled') {
   enableDarkMode();
 }
 
+chatButton.addEventListener('click', () => {
+  var chatWindow = document.getElementById('chatbot-window');
+  console.log(chatWindow.style.display)
+  chatWindow.style.display = chatWindow.style.display != 'block' ? 'block' : 'none';
+});
+
+chatbotClose.addEventListener('click', () => {
+  var chatWindow = document.getElementById('chatbot-window');
+  console.log("test closing")
+  chatWindow.style.display = chatWindow.style.display === 'none' ? 'block' : 'none';
+});
+
+
+let botMessage = document.createElement("div");
+botMessage.classList.add('chat-message', 'bot-message');
+botMessage.textContent = "Welcome to the bettEUR chatbot, what can I help you with";
+
+document.getElementById("chat-messages").appendChild(botMessage);
+
+let chatWindow = document.getElementById("chat-messages");
+chatWindow.scrollTop = chatWindow.scrollHeight;
+
+function appendMessage(message, isUserMessage) {
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add('chat-message');
+  if (isUserMessage) {
+      messageDiv.classList.add('user-message');
+  } else {
+      messageDiv.classList.add('bot-message');
+  }
+  messageDiv.textContent = message;
+  document.querySelector('.chat-messages').appendChild(messageDiv);
+}
+
 darkModeToggle.addEventListener('click', () => {
   darkMode = localStorage.getItem('darkMode'); 
 
@@ -29,18 +119,72 @@ darkModeToggle.addEventListener('click', () => {
 
 // Adjustable font-size
 
-(function(){
-    var fontSizeSlider = $("#font_size_slider");
-    var fontSizeValueText = $("#font_size_value");
-    var htmlElement = $("html");
-    var bodyElement = $("body");
-    fontSizeValueText.html("Base font size: " + bodyElement.css("font-size"));
-    fontSizeSlider.on("input change", changeFontSize);
+// (function(){
+//     var fontSizeSlider = $("#font_size_slider");
+//     var fontSizeValueText = $("#font_size_value");
+//     var htmlElement = $("html");
+//     var bodyElement = $("body");
+//     fontSizeValueText.html("Base font size: " + bodyElement.css("font-size"));
+//     fontSizeSlider.on("input change", changeFontSize);
     
-    function changeFontSize(e) {
-      var currentVal = fontSizeSlider.val();
-      htmlElement.css("font-size", currentVal + "px");
-      bodyElement.css("font-size", currentVal + "px");
-      fontSizeValueText.html("Base font size: " + currentVal + "px");
+//     function changeFontSize(e) {
+//       var currentVal = fontSizeSlider.val();
+//       htmlElement.css("font-size", currentVal + "px");
+//       bodyElement.css("font-size", currentVal + "px");
+//       fontSizeValueText.html("Base font size: " + currentVal + "px");
+//     }
+//   })();
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+
+  console.log("Page loaded")
+  console.log(stringSimilarity.compareTwoStrings('what!', 'who?'));
+
+  document.getElementById('chat-input').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        document.getElementById('send-message').click();
     }
-  })();
+  });
+  
+
+  document.getElementById('send-message').addEventListener('click', function() {
+      const inputField = document.getElementById('chat-input');
+      const chatMessages = document.getElementById('chat-messages');
+
+
+      user_input = inputField.value
+
+      if (user_input.length == 0) {
+          return
+      }
+
+      
+      
+      const userMessageDiv = document.createElement('div');
+      userMessageDiv.textContent = user_input;
+      
+      userMessageDiv.classList.add('chat-message', 'user-message');
+      chatMessages.appendChild(userMessageDiv);
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+
+      setTimeout(function() {
+          const botMessageDiv = document.createElement('div');
+          
+          console.log("inputfield: " + user_input)
+          console.log("inputfield: " + stringSimilarity.compareTwoStrings("Hello", user_input))
+
+          similarity = questions.map(db_question => stringSimilarity.compareTwoStrings(db_question, user_input))
+          console.log(similarity)
+          console.log(Math.max(...similarity))
+          botMessageDiv.textContent = answers[similarity.indexOf(Math.max(...similarity))]
+
+          botMessageDiv.classList.add('chat-message', 'bot-message');
+          chatMessages.appendChild(botMessageDiv);
+          chatMessages.scrollTop = chatMessages.scrollHeight;
+      }, 1000); 
+
+      inputField.value = ""
+  });
+});
+
